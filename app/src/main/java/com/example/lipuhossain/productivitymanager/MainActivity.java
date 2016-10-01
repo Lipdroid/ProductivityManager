6bf2b65e65428ae88f4121aa92f425295aca1aaf
Lipu Hossain <lipuhossain@mds-MacBook-Pro.local>
2016-10-01 18:58:42 +0600

    design 30% complete

--- app/src/main/java/com/example/lipuhossain/productivitymanager/MainActivity.java
+++ app/src/main/java/com/example/lipuhossain/productivitymanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lipuhossain.productivitymanager;
 
+import android.content.pm.ActivityInfo;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,22 +12,34 @@ import com.example.lipuhossain.productivitymanager.utils.GlobalUtils;
 
 import java.util.ArrayList;
 
+import io.netopen.hotbitmapgg.library.view.RingProgressBar;
+
 public class MainActivity extends AppCompatActivity {
 
     private BookNowDateListAdapter adapter = null;
     private ViewPager pager = null;
     ArrayList<CustomDate> mListDate = null;
+    RingProgressBar progress_estimated_productivity = null;
+    RingProgressBar progress_actual_productivity = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
         findViewByIds();
         initViewPager();
+        // Set the progress bar's progress
+        progress_estimated_productivity.setProgress(88);
+        progress_actual_productivity.setProgress(26);
     }
 
     private void findViewByIds() {
         pager = (ViewPager) findViewById(R.id.date_pager);
+        progress_estimated_productivity = (RingProgressBar) findViewById(R.id.estimated_progress);
+        progress_actual_productivity = (RingProgressBar) findViewById(R.id.actual_progress);
+
     }
 
     private void initViewPager() {
